d9e9bdaf1afd6e86d2c9e37241bccd144869efd6
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-04-30 14:14:33 +0300

    first commit\testing

---
+++ Login_v16/js/checkPass.js
@@ -0,0 +1,26 @@
+/*function validate() {
+    $("#btn-submit").attr("disabled",true);
+    if($("#password") == "hello") {
+        $("#btn-submit").attr("disabled",false);
+    }
+}*/
+
+
+/*$("input[type='password']").on("input", function () {
+    validate();
+});
+function validate(){
+    var show = true;
+    $("input[type='password']").each(function(){
+        if($(this).val()==''){
+            show = false;
+        }
+    });
+    if(show){
+        $('#btn-submit').css({cursor:'pointer'})
+        $('#btn-submit').removeAttr('disabled')
+    }
+    else {
+        $('#btn-submit').css({cursor:'not-allowed'})
+    }
+};*/
