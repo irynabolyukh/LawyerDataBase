871770b45daf3828cdb7943f09b19cddb7d29528
d4tAloUh <d4tAloUh@users.noreply.github.com>
2020-05-04 22:05:44 +0300

    setting up djangp

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, "docs"),
+        path: path.resolve(__dirname, "LawyerDataBase/DataBase/docs"),
         filename: 'js/main.js',
     },
     module: {
