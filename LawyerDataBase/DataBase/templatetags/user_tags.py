47a7b28077a38be6654feb8a8450a105a1066433
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-05-18 20:18:21 +0300

    some btns&links hidden from Lawyers and Clients

---
+++ LawyerDataBase/DataBase/templatetags/user_tags.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django import template
+
+register = template.Library()
+
+@register.filter('has_group')
+def has_group(user, group_name):
+    groups = user.groups.all().values_list('name', flat=True)
+    return True if group_name in groups else False
