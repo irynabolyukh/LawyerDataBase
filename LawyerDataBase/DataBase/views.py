9f25a2574b3083b1a21dc817a987c78b0f7ea585
d4tAloUh <d4tAloUh@users.noreply.github.com>
2020-06-15 11:46:44 +0300

    added selectpicker to appointments

--- LawyerDataBase/DataBase/views.py
+++ LawyerDataBase/DataBase/views.py
@@ -1068,11 +1068,10 @@ class ServicesListView(ListView):
         service_id = self.request.GET.get('service_id', '')
         se_name = self.request.GET.get('se_name', '')
         group_sel = self.request.GET.get('group', '')
-        data['object_list'] = Services.objects.filter(active=True).filter(service_code__icontains=service_id).\
-                                                filter(service_group__name_group__icontains=group_sel).\
-                                                filter(name_service__icontains=se_name)
-
-
+        data['object_list'] = Services.objects.filter(active=True,
+                                                      service_code__icontains=service_id,
+                                                      service_group__name_group__icontains=group_sel,
+                                                      name_service__icontains=se_name)
         data['service_id'] = service_id
         data['se_name'] = se_name
         data['group_sel'] = group_sel
